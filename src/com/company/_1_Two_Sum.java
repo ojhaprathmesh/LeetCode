8f64c5121fa43906bac5740b3d350c99b70d3b8d
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-09-05 12:01:05 +0530

    Solved LeetCode Problem: 1(Python and Java)

--- src/com/company/_1_Two_Sum.java
+++ src/com/company/_1_Two_Sum.java
@@ -1,27 +1,33 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _1_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
-        nums = Arrays.stream(nums).sorted().toArray();
-        int least = 1;
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < target) {
-                least = i + 1;
+            int num = nums[i];
+            int complement = target - num;
+
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{numToIndex.get(complement), i};
             }
+
+            numToIndex.put(num, i);
         }
 
-        int[] sol = new int[least];
-        int count = 0;
-        for (int i = 0; i < least; i++) {
-
-        }
-        return sol;
+        // In case no solution is found, throw an exception or return an empty array
+        return new int[]{};
     }
 
     public static void main(String[] args) {
-        _1_Two_Sum obj = new _1_Two_Sum();
-        System.out.println(Arrays.toString(obj.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        _1_Two_Sum solution = new _1_Two_Sum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 18;
+
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
     }
 }
