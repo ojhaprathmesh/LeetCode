162381de7b7ef6f964c71da73293ac3a74bd703b
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-09-10 15:55:53 +0530

    Solved LeetCode Problem: 28

---
+++ src/com/company/_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
